01b2b9ec5d1a966f19e37f20113300f20aef6b4f
Richard <richard@stromer.org>
2024-07-29 11:16:46 +0200

    add wrapper func and create without model

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import instructor
+from instructor.client import T
 from langfuse.openai import openai
 from pydantic import BaseModel
 
-client = instructor.from_openai(openai.Client())
+client = instructor.from_openai(openai.Client(api_key=""))
 
 
 class SampleSchema(BaseModel):
@@ -15,3 +16,21 @@ result = client.completions.chat.create(
     model="gpt-3.5-turbo",
     response_model=SampleSchema,
 )
+
+result2 = client.completions.chat.create(
+    messages=[],
+    model="gpt-3.5-turbo",
+    response_model=None,
+)
+
+
+def wrapped_create(response_model: type[T] | None) -> T | str:
+    result = client.create(
+        messages=[],
+        model="gpt-3.5-turbo",
+        response_model=response_model,
+    )
+    if response_model is None:
+        return result.choices[0].message.content or ""
+
+    return result
